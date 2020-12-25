dd5dfd31a6bf777dd5324bc3f938e4edf2a115ee
CallMeRZIBI <michalenko.jakub@gmail.com>
2020-12-25 23:24:50 +0100

    added key_listening

--- data_collection.py
+++ data_collection.py
@@ -1,22 +1,29 @@
 import numpy as np
-from PIL import ImageGrab
 import mss
 import cv2
 import time
+import keyboard
 
 monitor = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
+to_break = False
 
-def screen_record():
-    last_time = time.time()
+def get_screen():
+    global to_break
     with mss.mss() as sct:
-        while(True):
-            printscreen = np.array(sct.grab(monitor))
-            printscreen = cv2.resize(printscreen,(256,144))
-            print("loop took {} seconds".format(time.time()-last_time))
-            last_time = time.time()
-            cv2.imshow('window', printscreen)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
+        printscreen = np.array(sct.grab(monitor))
+        printscreen = cv2.resize(printscreen,(256,144))
+        cv2.imshow('window', printscreen)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            keyboard.unhook_all()
+            to_break = True
 
-screen_record()
+def get_key():
+    presses = [keyboard.is_pressed('w'),keyboard.is_pressed('a'),keyboard.is_pressed('s'),keyboard.is_pressed('d')]
+    return presses
+
+time.sleep(0)
+while to_break == False:
+    screen = get_screen()
+    keys = get_key()
+    print(keys)
