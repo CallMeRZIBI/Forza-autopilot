bd8240b82a0ce8880de96c3163f11a6eb76b23fc
CallMeRZIBI <michalenko.jakub@gmail.com>
2021-01-25 22:05:38 +0100

    remade the model for now, so it's learning from picture where you have box around detected objects instead from image and coords of boxes

--- load_data.py
+++ load_data.py
@@ -14,10 +14,100 @@ def detect_objects(image):
     return cvOut
 
 def get_objects(objects,image):
+    # Adding bounding box to array of detected objects
+    detected = []
+    for detection in objects[0,0,:,:]:
+        score = float(detection[2])
+        if score > 0.4:
+            left = int(detection[3] * image.shape[1])
+            top = int(detection[4] * image.shape[0])
+            right = int(detection[5] * image.shape[1])
+            bottom = int(detection[6] * image.shape[0])
+            detected.append([left, top, right, bottom])
+
+    return detected
+
+def draw_boxes(image, coords):
+    for detection in coords:
+        cv2.rectangle(image,(detection[0],detection[1],detection[2],detection[3]), (23,230,210), thickness=1)
+
+    return image
+
+def create_training_data():
+    try:
+        actual_file = 0
+        for files in os.listdir("collected_data"):
+            path = os.path.join("collected_data", files)
+            img_path = os.path.join(path, "images")
+            labels_path = os.path.join(path, "keys")
+
+            actual_file += 1
+            i = 0
+            while i < len(os.listdir(img_path)):
+                image = cv2.imread(os.path.join(img_path,"image{}.jpg".format(i)))
+                objects = detect_objects(image)
+
+                detected = get_objects(objects, image)
+
+                img = draw_boxes(image, detected)
+
+                gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                f = open(os.path.join(labels_path,"key{}.txt".format(i)), "r")
+                f = f.read()
+                label = f.split(',')
+                label = [int(label[0]), int(label[1]),int(label[2]), int(label[3])]
+
+                training_data.append([gray_image,label])
+                i+=1
+                
+                print("Loaded: {} out of {}, {} folder".format(i,len(os.listdir(img_path)),actual_file,len(files)))
+    except Exception as e:
+        print(e)
+
+create_training_data()
+# Maybye shuffle training data
+random.shuffle(training_data)
+
+#-----------------data loaded----------------
+
+X = []
+Y = []
+
+for image, label in training_data:
+    X.append(image)
+    Y.append(label)
+
+X = np.array(X).reshape(-1, 144, 256,1)
+Y = np.array(Y)
+
+# Saving data
+np.savez_compressed("training_data/data.npz",X,Y)
+
+
+
+
+
+#--------------------loading data for neural newtork with multiple networks added together----------------------------
+'''import numpy as np
+import os
+import cv2
+import random
+import tensorflow as tf
+
+training_data = []
+cvNet = cv2.dnn.readNetFromTensorflow('opencv_model/frozen_inference_graph.pb', 'opencv_model/model.pbtxt')
+
+def detect_objects(image):
+    image = cv2.cvtColor(image,cv2.COLOR_RGBA2RGB)
+    cvNet.setInput(cv2.dnn.blobFromImage(image, size=(256,144),swapRB=True,crop=False))
+    cvOut = cvNet.forward()
+    return cvOut
+
+def get_objects(objects,image):
     # This is awful, later on don't make max detections but differently sized arrays
     # Adding bounding box to array of detected objects
     detected = []
-    max_detections = 5
+    max_detections = 3
     actual_detection = 0
     for detection in objects[0,0,:,:]:
         score = float(detection[2])
@@ -33,7 +123,7 @@ def get_objects(objects,image):
 
     # When 5 things aren't detected then it will fill the rest with zeroes
     while actual_detection < max_detections:
-        detected.append([int(0),int(0),int(0),int(0)])
+        detected.append([None,None,None,None])
         actual_detection+=1
     detected = np.array(detected)
     return detected
@@ -88,3 +178,4 @@ Z = np.array(Z)
 
 # Saving data
 np.savez_compressed("training_data/data.npz",X,Y,Z)
+'''
--- train_model.py
+++ train_model.py
@@ -7,6 +7,78 @@ from tensorflow.keras.callbacks import TensorBoard
 import time
 
 # Loading data
+data = np.load("training_data/data.npz")
+X = data['arr_0']
+
+Y = data['arr_1']
+
+X = X/255.0
+
+#--------------Optimization--------------
+dense_layers = [1]
+layer_sizes = [64]
+conv_layers = [3]
+
+# Creating model for image processing
+model = Sequential()
+
+# Creating model for object processing
+model2 = Sequential()
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            Name = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size,dense_layer, int(time.time()))
+            print(Name)
+            # TensorBoard
+            tensorboard = TensorBoard(log_dir='logs/{}'.format(Name))
+
+            # Model for image processing
+            # First layer
+            model.add(Conv2D(layer_size, (3,3), input_shape = X.shape[1:]))
+            model.add(Activation("relu"))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+
+            for l in range(conv_layer-1):
+                # Second layer
+                model.add(Conv2D(layer_size, (3,3)))
+                model.add(Activation("relu"))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+            # Third layer
+            model.add(Flatten())
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+                model.add(Dropout(0.2))
+
+            model.add(Dense(4))
+            model.add(Activation('sigmoid'))
+
+            model.compile(loss="binary_crossentropy",
+            optimizer="adam",
+            metrics=['accuracy'])
+
+            prev_time = time.time()
+
+            model.fit(X,Y, batch_size=64,epochs=20, validation_split=0.1, callbacks=[tensorboard])
+            print("Training took: {}".format(int(time.time() - prev_time)))
+
+model.save("model/{}x{}x{}-CNN.model".format(layer_size,conv_layer,dense_layer))
+
+
+
+
+
+#------------------neural network with two networks added together-----------------------
+'''import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, Input
+from tensorflow.keras.callbacks import TensorBoard
+import time
+
+# Loading data
 data = np.load("training_data/data.npz",allow_pickle=True)
 X = data['arr_0']
 
@@ -64,11 +136,13 @@ for dense_layer in dense_layers:
 
             # Model for object processing
             # Inputs
-            model2.add(Input(shape=(5,4)))
+            model2.add(Input(shape=(3,4)))
             # Hidden layer
             model2.add(Dense(8))
             model2.add(Activation('relu'))
             model2.add(Flatten())
+            model2.add(Dense(8))
+            model2.add(Activation('relu'))
             # Outputs
             model2.add(Dense(4))
             model2.add(Activation('sigmoid'))
@@ -102,4 +176,4 @@ for dense_layer in dense_layers:
             print("Training took: {}".format(int(time.time() - prev_time)))
             mergedModel.save("model/64x3x1-CNN.model")
 
-#model.save("model/{}x{}x{}-CNN.model".format(layer_size,conv_layer,dense_layer))
+#model.save("model/{}x{}x{}-CNN.model".format(layer_size,conv_layer,dense_layer))'''
